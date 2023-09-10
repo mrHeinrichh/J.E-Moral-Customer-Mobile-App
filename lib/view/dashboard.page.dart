272de9fb76463c2d033a9b095fef01a91db09616
John Heinrich <johnheinrich.fabros@tup.edu.ph>
2023-09-10 18:23:56 +0800

    feat: :white_check_mark: Add dashboard page

---
+++ lib/view/dashboard.page.dart
@@ -0,0 +1,103 @@
+import 'package:customer_app/widgets/bottom_nav_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class DashboardPage extends StatefulWidget {
+  @override
+  _DashboardPageState createState() => _DashboardPageState();
+}
+
+class _DashboardPageState extends State<DashboardPage> {
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            buildTopBar(),
+            Expanded(
+              child: getPage(_currentIndex),
+            ),
+            BottomNavBar(
+              currentIndex: _currentIndex,
+              onTap: (index) {
+                setState(() {
+                  _currentIndex = index;
+                });
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildTopBar() {
+    return Container(
+      height: 100,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 35.0),
+        child: Row(
+          children: [
+            Expanded(
+              child: Material(
+                elevation: 5,
+                borderRadius: BorderRadius.circular(10.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10.0),
+                  child: Container(
+                    height: 45,
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                    ),
+                    child: const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 8.0),
+                      child: Row(
+                        children: [
+                          Icon(
+                            Icons.search,
+                            color: Color(0xFF232937),
+                          ),
+                          Expanded(
+                            child: TextField(
+                              decoration: InputDecoration(
+                                hintText: 'Search',
+                                border: InputBorder.none,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(width: 10),
+            Material(
+              elevation: 5,
+              borderRadius: BorderRadius.circular(10.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10.0),
+                child: IconButton(
+                  icon: const Icon(
+                    FontAwesomeIcons.cartShopping,
+                    color: Color(0xFF232937),
+                    size: 30,
+                  ),
+                  onPressed: () {},
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget getPage(int index) {
+    return Container();
+  }
+}
