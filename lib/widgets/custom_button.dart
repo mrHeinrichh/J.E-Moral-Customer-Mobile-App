297bc09634c3fa1bf129fe0867479cb6366ea144
John Heinrich <johnheinrich.fabros@tup.edu.ph>
2023-09-12 09:05:07 +0800

    feat: :white_check_mark: Add modular customized button

--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -64,3 +64,50 @@ class CustomWhiteButton extends StatelessWidget {
     );
   }
 }
+
+class CustomizedButton extends StatelessWidget {
+  final VoidCallback onPressed;
+  final String text;
+  final double height;
+  final double width;
+  final double fontz; // Add the type declaration 'double' here
+
+  CustomizedButton({
+    required this.onPressed,
+    required this.text,
+    required this.height,
+    required this.width,
+    required this.fontz, // Add the type declaration 'double' here
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        height: height,
+        width: width,
+        child: ElevatedButton(
+          onPressed: onPressed,
+          style: ElevatedButton.styleFrom(
+            backgroundColor: const Color(0xFF232937), // Background color
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30),
+            ),
+          ),
+          child: Container(
+            width: double.infinity,
+            alignment: Alignment.center,
+            child: Text(
+              text,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: fontz,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
