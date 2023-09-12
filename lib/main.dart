e509a8dc56da7d2c72fba2144bb5438591486361
John Heinrich <johnheinrich.fabros@tup.edu.ph>
2023-09-12 09:00:21 +0800

    feat: :white_check_mark: Add appointmentRoute

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:customer_app/routes/app_routes.dart';
+import 'package:customer_app/view/appointment.page.dart';
 import 'package:customer_app/view/dashboard.page.dart';
 import 'package:customer_app/view/login.page.dart';
 import 'package:customer_app/view/onboarding.page.dart';
@@ -21,6 +22,8 @@ class MyApp extends StatelessWidget {
         signupRoute: (context) => SignupPage(), // Use the imported route
         loginRoute: (context) => LoginPage(), // Use the imported route
         dashboardRoute: (context) => DashboardPage(), // Use the imported route
+        appointmentRoute: (context) =>
+            AppointmentPage(), // Use the imported route
       },
     );
   }
