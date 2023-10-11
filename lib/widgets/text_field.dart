6422d3ea0442296bc3449a3e73dcef53633f8157
John Heinrich <johnheinrich.fabros@tup.edu.ph>
2023-10-11 08:32:22 +0800

    feat: :art: Modularize customTextField

--- lib/widgets/text_field.dart
+++ lib/widgets/text_field.dart
@@ -13,8 +13,8 @@ class CustomTextField extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin:
-        const  EdgeInsets.symmetric(vertical: 4.0), // Set your desired margin here
+      margin: const EdgeInsets.symmetric(
+          vertical: 4.0), // Set your desired margin here
       child: TextField(
         decoration: InputDecoration(
           labelText: labelText,
@@ -22,6 +22,36 @@ class CustomTextField extends StatelessWidget {
           hintText: hintText,
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(40.0),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomTextField1 extends StatelessWidget {
+  final String labelText;
+  final String hintText;
+  final double borderRadius; // Add a new parameter for border radius
+
+  const CustomTextField1({
+    Key? key,
+    required this.labelText,
+    required this.hintText,
+    this.borderRadius = 10.0, // Default border radius is 40.0
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 4.0),
+      child: TextField(
+        decoration: InputDecoration(
+          labelText: labelText,
+          labelStyle: const TextStyle(fontSize: 15.0),
+          hintText: hintText,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(borderRadius),
           ),
         ),
       ),
