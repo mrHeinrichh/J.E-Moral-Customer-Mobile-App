2995dbba21776e0726b3547bcd737c03edde58de
John Heinrich <johnheinrich.fabros@tup.edu.ph>
2023-10-13 22:24:43 +0800

    refactor: :lipstick: Add function to textfield and textbutton

--- lib/view/set_delivery.page.dart
+++ lib/view/set_delivery.page.dart
@@ -1,17 +1,63 @@
+import 'package:customer_app/widgets/location_button.dart';
+import 'package:customer_app/widgets/location_search.dart';
 import 'package:customer_app/widgets/custom_button.dart';
 import 'package:customer_app/widgets/text_field.dart';
 import 'package:flutter/material.dart';
-
-enum DeliveryType { standard, express } // Define enum for delivery type
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class SetDeliveryPage extends StatefulWidget {
   @override
   _SetDeliveryPageState createState() => _SetDeliveryPageState();
 }
 
+enum PaymentMethod { cashOnDelivery, gcashPayment }
+
+enum AssemblyOption { yes, no }
+
 class _SetDeliveryPageState extends State<SetDeliveryPage> {
   String? selectedLocation;
-  DeliveryType? selectedDeliveryType; // Store selected delivery type
+  PaymentMethod? selectedPaymentMethod;
+  AssemblyOption? selectedAssemblyOption;
+  List<String> searchResults = [];
+  TextEditingController locationController = TextEditingController();
+
+  Future<void> fetchLocationData(String query) async {
+    if (query.isEmpty) {
+      setState(() {
+        searchResults = [];
+      });
+      return;
+    }
+    const apiKey = 'e8d77b2d8e7740c3b7727970dc8fc59e';
+    final apiUrl =
+        'https://api.geoapify.com/v1/geocode/autocomplete?text=$query&apiKey=$apiKey';
+
+    try {
+      final response = await http.get(Uri.parse(apiUrl));
+
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+
+        if (data.containsKey('features')) {
+          final features = data['features'] as List<dynamic>;
+          final results = features
+              .map((feature) => feature['properties']['formatted'] as String)
+              .toList();
+
+          setState(() {
+            searchResults = results;
+          });
+        } else {
+          print('No "features" key in the API response.');
+        }
+      } else {
+        print('API Request Error: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,143 +70,93 @@ class _SetDeliveryPageState extends State<SetDeliveryPage> {
           style: TextStyle(color: Color(0xFF232937), fontSize: 24),
         ),
       ),
-      body: Padding(
-        padding: EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              "Set Delivery Location",
-            ),
-            SizedBox(height: 20),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 12),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10.0),
-                border: Border.all(color: Colors.grey),
-              ),
-              child: DropdownButtonFormField<String>(
-                value: selectedLocation,
-                onChanged: (newValue) {
-                  setState(() {
-                    selectedLocation = newValue;
-                  });
-                },
-                items: <String?>[
-                  'Location 1',
-                  'Location 2',
-                  'Location 3',
-                  'Location 4',
-                  'Location 5',
-                ].map<DropdownMenuItem<String>>((String? value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value ?? ''),
-                  );
-                }).toList(),
-                decoration: InputDecoration(
-                  hintText: "Select a Location",
-                  border: InputBorder.none,
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Column(
+              children: [
+                const Text(
+                  "Set Delivery Location",
                 ),
-              ),
-            ),
-            SizedBox(height: 20),
-            Container(
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: Color(0xFFD9D9D9), // Background color D9D9D9
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: TextButton(
-                onPressed: () {
-                  // Add your action when the button is pressed
-                },
-                child: Text(
-                  "Use my current location",
-                  style: TextStyle(color: Colors.black),
+                const SizedBox(height: 20),
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 12),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    border: Border.all(color: Colors.grey),
+                  ),
+                  child: Column(
+                    children: [
+                      LocationSearchWidget(
+                        locationController: locationController,
+                        onLocationChanged: (query) {
+                          fetchLocationData(query);
+                        },
+                      ),
+                      ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: searchResults.length,
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            title: Text(searchResults[index]),
+                            onTap: () {
+                              setState(() {
+                                selectedLocation = searchResults[index];
+                                locationController.text = searchResults[index];
+                                searchResults = [];
+                              });
+                            },
+                          );
+                        },
+                      ),
+                    ],
+                  ),
                 ),
+                const SizedBox(height: 20),
+                LocationButtonWidget(
+                  onLocationChanged: (String address) {
+                    locationController.text = address;
+                  },
+                ),
+                const CustomTextField1(
+                  labelText: 'Name',
+                  hintText: 'Enter your Name',
+                ),
+                const CustomTextField1(
+                  labelText: 'Contact Number',
+                  hintText: 'Enter your contact number',
+                ),
+                const CustomTextField1(
+                  labelText: 'House#/Lot/Blk',
+                  hintText: 'Enter your house number',
+                ),
+                const SizedBox(height: 20),
+                const Text("Choose Payment Method"),
+                // Radio buttons and other UI elements
+              ],
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CustomizedButton(
+                onPressed: () {},
+                text: 'Scheduled',
+                height: 50,
+                width: 180,
+                fontz: 20,
               ),
-            ),
-            CustomTextField1(
-              labelText: 'Name',
-              hintText: 'Enter your Name',
-            ),
-            CustomTextField1(
-              labelText: 'Contact Number',
-              hintText: 'Enter your contact number',
-            ),
-            CustomTextField1(
-              labelText: 'House#/Lot/Blk',
-              hintText: 'Enter your house number',
-            ),
-            SizedBox(height: 20),
-            // Add radio buttons for delivery type
-            Text("Choose Payment Method"),
-            ListTile(
-              title: const Text('Cash On Delivery'),
-              leading: Radio<DeliveryType>(
-                value: DeliveryType.standard,
-                groupValue: selectedDeliveryType,
-                onChanged: (DeliveryType? value) {
-                  setState(() {
-                    selectedDeliveryType = value;
-                  });
-                },
+              CustomizedButton(
+                onPressed: () {},
+                text: 'Deliver Now',
+                height: 50,
+                width: 180,
+                fontz: 20,
               ),
-            ),
-            ListTile(
-              title: const Text('Gcash Payment'),
-              leading: Radio<DeliveryType>(
-                value: DeliveryType.express,
-                groupValue: selectedDeliveryType,
-                onChanged: (DeliveryType? value) {
-                  setState(() {
-                    selectedDeliveryType = value;
-                  });
-                },
-              ),
-            ),
-            Text("Needs to be assembled?"),
-            ListTile(
-              title: const Text('Yes'),
-              leading: Radio<DeliveryType>(
-                value: DeliveryType.standard,
-                groupValue: selectedDeliveryType,
-                onChanged: (DeliveryType? value) {
-                  setState(() {
-                    selectedDeliveryType = value;
-                  });
-                },
-              ),
-            ),
-            ListTile(
-              title: const Text('No'),
-              leading: Radio<DeliveryType>(
-                value: DeliveryType.express,
-                groupValue: selectedDeliveryType,
-                onChanged: (DeliveryType? value) {
-                  setState(() {
-                    selectedDeliveryType = value;
-                  });
-                },
-              ),
-            ),
-            CustomizedButton(
-              onPressed: () {},
-              text: 'Scheduled',
-              height: 60,
-              width: 260,
-              fontz: 20,
-            ),
-            CustomizedButton(
-              onPressed: () {},
-              text: 'Deliver Now',
-              height: 60,
-              width: 260,
-              fontz: 20,
-            ),
-          ],
-        ),
+            ],
+          ),
+        ],
       ),
     );
   }
