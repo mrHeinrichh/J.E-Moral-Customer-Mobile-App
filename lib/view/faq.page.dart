920814aac40124f52b1ac21e280b4bc2508a4db9
Anthony-CGM <anthonycharles.madrid@tup.edu.ph>
2023-12-05 04:02:41 +0800

    faq-Q6

--- lib/view/faq.page.dart
+++ lib/view/faq.page.dart
@@ -12,12 +12,13 @@ class _FaqPageState extends State<FaqPage> {
     'How to Apply as a Rider?',
     'How to Order?',
     'Mode of Payment',
+    'Can I track my order?',
+
     // 'Support Request',
     // 'My Account',
     // 'Orders and Payment',
     // 'Get Help With My Pay',
     // 'Safety Concerns',
-    // 'How to Become a Rider',
     // 'How to Become a Retailer',
     // 'Selling and Billing',
     // 'Refilling',
@@ -27,12 +28,11 @@ class _FaqPageState extends State<FaqPage> {
 
   List<String> answers = [
     'We, the J.E. Moral LPG Store is committed to help our growing clients by giving them a platform to access our products anytime and anywhere to ease their purchasing time and efforts.',
-    'To access the full potential of the application according to your role, just simply register or create an account, supply the necessary requirements, and wait for the confirmation of your account.',
+    'To access the full potential of the application according to your role, just simply register or create an account, supply the necessary requirements, and wait for the confirmation of your account. Discounts awaits for Senior Citizens and PWD. Just provide your Identification Cards upon registering.',
     'To apply as a Rider of our shop, just click "Book an Appointment" below "Apply as a Rider?" question in the landing page, supply the appointment information needed, and wait for the approval from the administrator confirming your schedule.',
     'For you to order in the application, just search or click to your desired products, add it to your cart, review, and proceed to checkout by supplying the necessary delivery information provided.',
     'The application offers you a Cash on Delivery and contactless payment such as GCash.',
-    // 'Answer 4',
-    // 'Answer 5',
+    'Yes, you can track your order once it is shipped. Just go to History -> View Orders -> Track Order.'
     // 'Answer 6',
     // 'Answer 7',
     // 'Answer 8',
