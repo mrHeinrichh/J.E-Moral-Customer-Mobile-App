35441214679f4b07b8d6a9f63b4838410fca4862
Heinrich Fabros <johnheinrich.fabros@tup.edu.ph>
2024-01-07 12:43:43 +0800

    Update api image upload to server

--- lib/view/profile.page.dart
+++ lib/view/profile.page.dart
@@ -1,8 +1,14 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:customer_app/view/user_provider.dart';
+
+import 'package:http_parser/http_parser.dart';
 
 class ProfilePage extends StatefulWidget {
   @override
@@ -16,13 +22,85 @@ class _ProfilePageState extends State<ProfilePage> {
   TextEditingController _addressController = TextEditingController();
   TextEditingController _emailController = TextEditingController();
   TextEditingController _passwordController = TextEditingController();
-  @override
-  void initState() {
-    super.initState();
-    // Access the UserProvider to get the userId
-    String? userId = Provider.of<UserProvider>(context, listen: false).userId;
-    // Initialize the Future in initState
-    _userDetailsFuture = fetchUserDetails(userId!);
+  File? _image;
+
+  Future<void> _pickImage() async {
+    final pickedFile =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      setState(() {
+        _image = File(pickedFile.path);
+      });
+
+      // Automatically upload the selected image
+      await _uploadSelectedImage();
+    }
+
+    print(_image);
+  }
+
+  Future<void> _uploadSelectedImage() async {
+    if (_image != null) {
+      final serverResponse = await uploadImageToServer(_image!);
+
+      if (serverResponse != null) {
+        // Handle the response as needed
+        print('Image uploaded to server successfully');
+        print(serverResponse);
+      } else {
+        print('Image upload to server failed');
+      }
+    }
+  }
+
+  Future<Map<String, dynamic>?> uploadImageToServer(File imageFile) async {
+    try {
+      var request = http.MultipartRequest(
+        'POST',
+        Uri.parse('https://lpg-api-06n8.onrender.com/api/v1/upload/image'),
+      );
+
+      var fileStream = http.ByteStream(Stream.castFrom(imageFile.openRead()));
+      var length = await imageFile.length();
+
+      String fileExtension = imageFile.path.split('.').last.toLowerCase();
+      var contentType = MediaType('image', 'png');
+
+      Map<String, String> imageExtensions = {
+        'png': 'png',
+        'jpg': 'jpeg',
+        'jpeg': 'jpeg',
+        'gif': 'gif',
+      };
+
+      if (imageExtensions.containsKey(fileExtension)) {
+        contentType = MediaType('image', imageExtensions[fileExtension]!);
+      }
+
+      var multipartFile = http.MultipartFile(
+        'image',
+        fileStream,
+        length,
+        filename: 'image.$fileExtension',
+        contentType: contentType,
+      );
+
+      request.files.add(multipartFile);
+
+      var response = await request.send();
+      if (response.statusCode == 200) {
+        final responseBody = await response.stream.bytesToString();
+        final parsedResponse = json.decode(responseBody);
+        return parsedResponse;
+      } else {
+        print("Image upload failed with status code: ${response.statusCode}");
+        return null;
+      }
+    } catch (e) {
+      print("Image upload failed with error: $e");
+      return null;
+    }
   }
 
   Future<void> updateUserProfile(
@@ -34,12 +112,24 @@ class _ProfilePageState extends State<ProfilePage> {
     String password,
   ) async {
     try {
+      final userData = await fetchUserDetails(userId);
+
+      if (_image != null) {
+        final serverResponse = await uploadImageToServer(_image!);
+
+        if (serverResponse != null) {
+          final imageUrl = serverResponse['data'][0]['path'];
+          userData["image"] = imageUrl;
+        }
+      }
+
       final response = await http.patch(
         Uri.parse('https://lpg-api-06n8.onrender.com/api/v1/users/$userId'),
         headers: {
           'Content-Type': 'application/json',
         },
         body: jsonEncode({
+          'image': userData["image"],
           'name': name,
           'contactNumber': contactNumber,
           'type': "Customer",
@@ -51,30 +141,17 @@ class _ProfilePageState extends State<ProfilePage> {
         }),
       );
 
-      print(name);
-      print(contactNumber);
-      print(address);
-      print(email);
-      print(password);
-      print('User ID: $userId');
-
       if (response.statusCode == 200) {
-        // User updated successfully
         print('User updated successfully');
         print('Response.statusCode: ${response.statusCode}');
         print(response.body);
 
-        // Reload user details
-        setState(() {
-          // Trigger a rebuild of the widget with updated data
-        });
+        setState(() {});
       } else {
-        // Handle error
         print('Failed to update user details: ${response.statusCode}');
-        print(response.body); // Print the response body for debugging
+        print(response.body);
       }
     } catch (error) {
-      // Handle error
       print('Error updating user details: $error');
     }
   }
@@ -99,6 +176,13 @@ class _ProfilePageState extends State<ProfilePage> {
       print('Error fetching user details: $error');
       throw Exception('Failed to load user details');
     }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    String? userId = Provider.of<UserProvider>(context, listen: false).userId;
+    _userDetailsFuture = fetchUserDetails(userId!);
   }
 
   Widget buildProfileWidget(Map<String, dynamic> userDetails, String? userId) {
@@ -176,7 +260,6 @@ class _ProfilePageState extends State<ProfilePage> {
                 ),
                 ElevatedButton(
                   onPressed: () async {
-                    // Show dialog on button click
                     showDialog(
                       context: context,
                       builder: (BuildContext context) {
@@ -186,43 +269,78 @@ class _ProfilePageState extends State<ProfilePage> {
                         _addressController.text = userData['address'] ?? '';
                         _emailController.text =
                             authData != null ? authData['email'] ?? '' : '';
-                        _passwordController.text =
-                            ''; // You may set the password if available
+                        _passwordController.text = '';
 
                         return AlertDialog(
                           title: Text('Edit your profile Data'),
-                          content: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              TextField(
-                                controller: _nameController,
-                                decoration: InputDecoration(labelText: 'Name'),
-                              ),
-                              TextField(
-                                controller: _contactNumberController,
-                                decoration: InputDecoration(
-                                    labelText: 'Contact Number'),
-                              ),
-                              TextField(
-                                controller: _addressController,
-                                decoration:
-                                    InputDecoration(labelText: 'Address'),
-                              ),
-                              TextField(
-                                controller: _emailController,
-                                decoration: InputDecoration(labelText: 'Email'),
-                              ),
-                              TextField(
-                                controller: _passwordController,
-                                decoration:
-                                    InputDecoration(labelText: 'Password'),
-                              ),
-                            ],
+                          content: StatefulBuilder(
+                            builder:
+                                (BuildContext context, StateSetter setState) {
+                              return SingleChildScrollView(
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    _image == null
+                                        ? const CircleAvatar(
+                                            radius: 50,
+                                            backgroundColor: Colors.grey,
+                                            child: Icon(
+                                              Icons.person,
+                                              color: Colors.white,
+                                              size: 50,
+                                            ),
+                                          )
+                                        : CircleAvatar(
+                                            radius: 50,
+                                            backgroundImage: FileImage(_image!),
+                                          ),
+                                    TextButton(
+                                      onPressed: () async {
+                                        await _pickImage();
+                                        setState(
+                                            () {}); // Rebuild the content after picking image
+                                      },
+                                      child: const Text(
+                                        "Upload Image",
+                                        style: TextStyle(
+                                          color: Colors.blue,
+                                          fontSize: 15.0,
+                                        ),
+                                      ),
+                                    ),
+                                    TextField(
+                                      controller: _nameController,
+                                      decoration:
+                                          InputDecoration(labelText: 'Name'),
+                                    ),
+                                    TextField(
+                                      controller: _contactNumberController,
+                                      decoration: InputDecoration(
+                                          labelText: 'Contact Number'),
+                                    ),
+                                    TextField(
+                                      controller: _addressController,
+                                      decoration:
+                                          InputDecoration(labelText: 'Address'),
+                                    ),
+                                    TextField(
+                                      controller: _emailController,
+                                      decoration:
+                                          InputDecoration(labelText: 'Email'),
+                                    ),
+                                    TextField(
+                                      controller: _passwordController,
+                                      decoration: InputDecoration(
+                                          labelText: 'Password'),
+                                    ),
+                                  ],
+                                ),
+                              );
+                            },
                           ),
                           actions: [
                             ElevatedButton(
                               onPressed: () async {
-                                // Handle the data entered in the dialog
                                 String name = _nameController.text;
                                 String contactNumber =
                                     _contactNumberController.text;
@@ -230,10 +348,8 @@ class _ProfilePageState extends State<ProfilePage> {
                                 String email = _emailController.text;
                                 String password = _passwordController.text;
 
-                                // Close the dialog
                                 Navigator.of(context).pop();
 
-                                // Update user profile
                                 await updateUserProfile(userId!, name,
                                     contactNumber, address, email, password);
                               },
@@ -241,7 +357,6 @@ class _ProfilePageState extends State<ProfilePage> {
                             ),
                             ElevatedButton(
                               onPressed: () {
-                                // Close the dialog without saving
                                 Navigator.of(context).pop();
                               },
                               child: Text('Cancel'),
@@ -263,9 +378,7 @@ class _ProfilePageState extends State<ProfilePage> {
 
   @override
   Widget build(BuildContext context) {
-    // Access the UserProvider to get the userId
     String? userId = Provider.of<UserProvider>(context).userId;
-    // Check if userId is available
     if (userId != null) {
       return FutureBuilder<Map<String, dynamic>>(
         future: _userDetailsFuture,
@@ -285,7 +398,6 @@ class _ProfilePageState extends State<ProfilePage> {
         },
       );
     } else {
-      // Handle the case where userId is null
       return Text('User ID not available');
     }
   }
