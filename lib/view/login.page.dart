9ea8025358c55e131f6eb80c4b0003ac3d54484c
mrHeinrichh <johnheinrich.fabros@tup.edu.ph>
2024-01-14 00:52:06 +0800

    Add loading in login function

--- lib/view/login.page.dart
+++ lib/view/login.page.dart
@@ -15,7 +15,6 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   TextEditingController emailController = TextEditingController(text: '');
   TextEditingController passwordController = TextEditingController(text: '');
-
   Future<Map<String, dynamic>> login(String? email, String? password,
       [BuildContext? context]) async {
     if (email == null || password == null) {
@@ -26,6 +25,11 @@ class _LoginPageState extends State<LoginPage> {
     print('Email: $email, Password: $password');
 
     try {
+      if (context != null) {
+        // Show loading indicator
+        Provider.of<UserProvider>(context, listen: false).setLoading(true);
+      }
+
       final response = await http.post(
         Uri.parse('https://lpg-api-06n8.onrender.com/api/v1/auth/login'),
         headers: {
@@ -36,6 +40,11 @@ class _LoginPageState extends State<LoginPage> {
           'password': password,
         }),
       );
+
+      if (context != null) {
+        // Hide loading indicator
+        Provider.of<UserProvider>(context, listen: false).setLoading(false);
+      }
 
       if (response.statusCode == 200) {
         final Map<String, dynamic> data = jsonDecode(response.body);
@@ -52,6 +61,12 @@ class _LoginPageState extends State<LoginPage> {
               print('User ID: $userId');
 
               // Fetch additional user details using the user ID
+              if (context != null) {
+                // Show loading indicator
+                Provider.of<UserProvider>(context, listen: false)
+                    .setLoading(true);
+              }
+
               final userDetailsResponse = await http.get(
                 Uri.parse(
                     'https://lpg-api-06n8.onrender.com/api/v1/users/$userId'),
@@ -59,6 +74,12 @@ class _LoginPageState extends State<LoginPage> {
                   'Content-Type': 'application/json',
                 },
               );
+
+              if (context != null) {
+                // Hide loading indicator
+                Provider.of<UserProvider>(context, listen: false)
+                    .setLoading(false);
+              }
 
               print('User Details Response: ${userDetailsResponse.statusCode}');
 
@@ -70,8 +91,6 @@ class _LoginPageState extends State<LoginPage> {
 
                 // Extract "type" field from user details
                 String userType = userDetailsData['data']['user']['__t'] ?? '';
-                print('User Type: $userType');
-
                 print('User Type: $userType');
 
                 if (userType == 'Customer') {
@@ -113,6 +132,11 @@ class _LoginPageState extends State<LoginPage> {
         return {'error': 'Login failed'};
       }
     } catch (error, stackTrace) {
+      if (context != null) {
+        // Hide loading indicator on error
+        Provider.of<UserProvider>(context, listen: false).setLoading(false);
+      }
+
       print('Error: $error');
       print('Stack trace: $stackTrace');
       return {'error': 'An error occurred during login'};
@@ -122,88 +146,99 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(50.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const SizedBox(height: 60.0),
-                Image.network(
-                  'https://raw.githubusercontent.com/mrHeinrichh/J.E-Moral-cdn/main/assets/png/logo-main.png',
-                  width: 550.0,
-                  height: null,
-                ),
-                const SizedBox(height: 50.0),
-                TextField(
-                  controller: emailController,
-                  decoration: InputDecoration(
-                    labelText: "Email Address",
-                    hintText: "Enter your Email Address",
-                  ),
-                ),
-                TextField(
-                  controller: passwordController,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: "Password",
-                    hintText: "Enter your Password",
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [Text("Forgot your password?")],
-                ),
-                SizedBox(height: 30.0),
-                Column(
-                  children: [
-                    LoginButton(
-                      onPressed: () async {
-                        final loginResult = await login(
-                          emailController.text,
-                          passwordController.text,
-                          context,
-                        );
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.all(50.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    const SizedBox(height: 60.0),
+                    Image.network(
+                      'https://raw.githubusercontent.com/mrHeinrichh/J.E-Moral-cdn/main/assets/png/logo-main.png',
+                      width: 550.0,
+                      height: null,
+                    ),
+                    const SizedBox(height: 50.0),
+                    TextField(
+                      controller: emailController,
+                      decoration: InputDecoration(
+                        labelText: "Email Address",
+                        hintText: "Enter your Email Address",
+                      ),
+                    ),
+                    TextField(
+                      controller: passwordController,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: "Password",
+                        hintText: "Enter your Password",
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [Text("Forgot your password?")],
+                    ),
+                    SizedBox(height: 30.0),
+                    Column(
+                      children: [
+                        LoginButton(
+                          onPressed: () async {
+                            final loginResult = await login(
+                              emailController.text,
+                              passwordController.text,
+                              context,
+                            );
 
-                        print('Login Result: $loginResult');
+                            print('Login Result: $loginResult');
 
-                        if (loginResult.containsKey('error')) {
-                          showDialog(
-                            context: context,
-                            builder: (context) {
-                              return AlertDialog(
-                                title: Text('Login Failed'),
-                                content: Text(loginResult['error']),
-                                actions: [
-                                  TextButton(
-                                    onPressed: () {
-                                      Navigator.of(context).pop();
-                                    },
-                                    child: Text('OK'),
-                                  ),
-                                ],
+                            if (loginResult.containsKey('error')) {
+                              showDialog(
+                                context: context,
+                                builder: (context) {
+                                  return AlertDialog(
+                                    title: Text('Login Failed'),
+                                    content: Text(loginResult['error']),
+                                    actions: [
+                                      TextButton(
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                        },
+                                        child: Text('OK'),
+                                      ),
+                                    ],
+                                  );
+                                },
                               );
-                            },
-                          );
-                        } else {
-                          Navigator.pushNamed(context, dashboardRoute);
-                        }
-                      },
-                    ),
-                    SizedBox(height: 16),
-                    CustomWhiteButton(
-                      onPressed: () {
-                        Navigator.pushNamed(context, onboardingRoute);
-                      },
-                      text: "Back",
-                    ),
+                            } else {
+                              Navigator.pushNamed(context, dashboardRoute);
+                            }
+                          },
+                        ),
+                        SizedBox(height: 16),
+                        CustomWhiteButton(
+                          onPressed: () {
+                            Navigator.pushNamed(context, onboardingRoute);
+                          },
+                          text: "Back",
+                        ),
+                      ],
+                    )
                   ],
-                )
-              ],
+                ),
+              ),
             ),
           ),
-        ),
+          if (Provider.of<UserProvider>(context).isLoading)
+            Container(
+              color: Colors.black.withOpacity(0.5),
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            ),
+        ],
       ),
     );
   }
