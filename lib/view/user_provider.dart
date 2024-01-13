7c2fc75bb894df2452281b6c38a52671955c64b8
mrHeinrichh <johnheinrich.fabros@tup.edu.ph>
2024-01-14 00:52:57 +0800

    Update user provider with loading state

--- lib/view/user_provider.dart
+++ lib/view/user_provider.dart
@@ -4,6 +4,14 @@ class UserProvider extends ChangeNotifier {
   String? _userId;
 
   String? get userId => _userId;
+  bool _isLoading = false;
+
+  bool get isLoading => _isLoading;
+
+  void setLoading(bool value) {
+    _isLoading = value;
+    notifyListeners();
+  }
 
   void setUserId(String userId) {
     _userId = userId;
